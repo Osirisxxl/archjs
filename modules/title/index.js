0660d8c21e2281d61f43c3c667a05cda0f8a6479
Jérémie Picard <jeremie.picard@zenika.com>
2022-09-10 22:19:06 +0200

    set linter up

--- modules/title/index.js
+++ modules/title/index.js
@@ -1,8 +1,9 @@
 import Component from "../render/Component";
 
 export default class Title extends Component {
-  render = () =>
-    `<div>
+  render() {
+    return `<div>
         <h1>Say hello to your first archjs setup!</h1>
      </div>`
+  }
 }
